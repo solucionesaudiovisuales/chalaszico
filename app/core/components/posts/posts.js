527275d3229a31899c40f7bf09b3b6cb21fc528d
Sabino Velasquez <sabinovelasquez@gmail.com>
2016-10-13 21:56:29 -0300

    base para animación

--- app/core/components/posts/posts.js
+++ app/core/components/posts/posts.js
@@ -15,7 +15,19 @@ export default ngModule => {
             this.posts.push(p);
           });
         });
-
+        this.like = () => {
+          // const b = Math.floor((Math.random() * 100) + 1);
+          // const d = ["flowOne", "flowTwo", "flowThree"];
+          // const a = ["colOne", "colTwo", "colThree", "colFour", "colFive", "colSix"];
+          // const c = (Math.random() * (1.6 - 1.2) + 1.2).toFixed(1);
+          // $('<div class="heart part-' + b + " " + a[Math.floor((Math.random() * 6))] + '" style="font-size:' + Math.floor(Math.random() * (50 - 22) + 22) + 'px;"><i class="fa fa-heart"></i></div>').appendTo(".hearts").css({
+          // animation: "" + d[Math.floor((Math.random() * 3))] + " " + c + "s linear"
+          // });
+          // $(".part-" + b).show();
+          // setTimeout(function() {
+          // $(".part-" + b).remove()
+          // }, c * 900);
+        };
         // this.getFullHtml = (untrusted) =>{
         //   console.log(untrusted)
         //   return $sce.getTrustedHtml(untrusted)
